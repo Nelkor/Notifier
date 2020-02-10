1b94299b8f25a933327a6c4437b4f81988ecbfe4
Nelkor <nelkor9855118751@gmail.com>
2020-02-10 21:34:50 +0300

    try to render links

--- src/components/not-card.ts
+++ src/components/not-card.ts
@@ -1,26 +1,40 @@
 import {
     LitElement,
+    TemplateResult,
+    CSSResult,
     html,
     css,
     customElement,
     property,
 } from 'lit-element';
 
+// TODO исправить
+const regex = /\[link (.+?)](.+?)\[\/link]/g;
+
 @customElement('not-card')
 export class NotCard extends LitElement
 {
-    static get styles()
+    static get styles(): CSSResult
     {
         return css`
+            .card {
+                width: 200px;
+                border: solid 1px;
+                padding: 20px;
+                border-radius: 4px;
+            }
         `;
     }
 
     @property({ type: String })
     content = '';
 
-    protected render()
+    protected render(): TemplateResult
     {
         return html`
+            <div class="card">
+                <span>${this.content}</span>
+            </div>
         `;
     }
 }
